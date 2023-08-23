50f71e44722987fca8e653e208c09ab094f28ecc
Novichenko Vitaliy <novichenkovitaliy@gmail.com>
2023-08-24 00:36:59 +0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Vitaliy Novichenko on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
